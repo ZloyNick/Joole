261efa9547be10657257a193c60b72070535bc9e
ZloyNick <sitdikov_r@magnit.ru>
2022-02-25 00:15:13 +0500

    Joole base impementation

---
+++ app/Application.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app;
+
+use zloynick\joole_framework\Application as BaseApplication;
+
+/**
+ * Class Application based on joole-framework's application class
+ */
+class Application extends BaseApplication
+{
+
+
+
+}
